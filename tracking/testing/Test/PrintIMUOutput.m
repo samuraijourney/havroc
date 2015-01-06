534f39592391f48451ee73881dc0173e7f42c7bb
Akram Kassay <samuraijourney@hotmail.com>
2015-01-06 17:23:48 -0500

    Updated matlab test code

--- tracking/testing/Test/PrintIMUOutput.m
+++ tracking/testing/Test/PrintIMUOutput.m
@@ -7,10 +7,32 @@ yaw = 0;
 pitch = 0;
 roll = 0;
 
-for i=1:1:10
-    yaw = [yaw fscanf(arduino,'%f')];
-    pitch = [pitch fscanf(arduino,'%f')];
-    roll = [roll fscanf(arduino,'%f')];
+point = [0 0 0];
+lineLength = 2;
+
+while(1)
+    figure(1);
+    clf;
+    hold on;
+    
+    yaw = fscanf(arduino,'%f')
+    pitch = fscanf(arduino,'%f')
+    roll = fscanf(arduino,'%f')
+    
+    x = lineLength*cosd(yaw)*cosd(pitch);
+    y = lineLength*sind(yaw)*cosd(pitch);
+    z = lineLength*sind(pitch);
+    
+    plot3([0 x],[0 y],[0 z]);
+    plot3(0,0,0,'r.');
+    plot3([0 x],[0 y],[-5 z],'r-');
+    
+    xlim([-5 5]);
+    ylim([-5 5]);
+    zlim([-5 5]);
+    grid on;
+    
+    view([-37.5 30]);
 end
 
 fclose(arduino);

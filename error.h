3dcd71a1c27620ea6cd955cbff868cb21d5fbd28
Akram Kassay <samuraijourney@hotmail.com>
2015-01-06 15:15:42 -0500

    Added error codes file

---
+++ error.h
@@ -0,0 +1,12 @@
+#ifndef H_ERROR
+#define H_ERROR
+
+// Tracking service layer errors
+#define SERVICE_START_SUCCESS 	1
+#define SERVICE_START_FAIL 		2
+#define SERVICE_END_SUCCESS		3
+#define SERVICE_END_FAIL		4
+#define SERVICE_KILLED			5
+#define SERVICE_READ_FAIL		6
+
+#endif

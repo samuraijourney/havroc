a4db482d8de59784752a3df01ff92f27f4fb53c4
Akram Kassay <samuraijourney@hotmail.com>
2015-01-19 14:30:37 -0500

    Should be cleaner

--- embedded/projects/cc3200/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"c:/ti/tirtos_simplelink_2_01_00_03/products/CC3200_driverlib_1.0.2/driverlib/ccs/Release/driverlib.a"
-

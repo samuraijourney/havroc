11091f7ea18a1e7ee0d4b21ba99bf8aa2f161787
Geo <geo.jp91@gmail.com>
2015-03-16 03:23:25 -0400

    minor uart updates to i2c

--- embedded/libs/havroc/communications/suit/suit_i2c.c
+++ embedded/libs/havroc/communications/suit/suit_i2c.c
@@ -39,7 +39,7 @@ SuitI2CErrorCode suit_i2c_transfer(uint8_t addr,
 	}
 
 	if (retVal != SUIT_I2C_E_SUCCESS)
-		Report("I2C Transfer Error\n");
+		Report("I2C Transfer Error\n\r");
 
     return retVal;
 }
@@ -56,7 +56,7 @@ SuitI2CErrorCode suit_i2c_read(uint8_t addr,
 	if (I2C_IF_ReadFrom(addr, writeBuff, 1, readBuff, readCount) < 0)
 	{
 		retVal = SUIT_I2C_E_BUS_FAULT;
-		Report("I2C Read Error\n");
+		Report("I2C Read Error\n\r");
 	}
 
 
@@ -80,7 +80,7 @@ SuitI2CErrorCode suit_i2c_write(uint8_t addr,
 	if (I2C_IF_Write(addr, writeBuff2, writeCount + 1, true) < 0)
 	{
 		retVal = SUIT_I2C_E_BUS_FAULT;
-		Report("I2C Write Error\n");
+		Report("I2C Write Error\n\r");
 	}
 
 	Memory_free(NULL, writeBuff2, writeCount + 1);

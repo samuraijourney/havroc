a67a0c336f1b1140ee39523201699d0bd5a945bf
Geo <geo.jp91@gmail.com>
2015-03-26 02:44:16 -0400

    -updated motor lib with waveforms

--- embedded/include/havroc/actuation/motor.h
+++ embedded/include/havroc/actuation/motor.h
@@ -8,47 +8,17 @@
 #include "stdint.h"
 #include "havroc/communications/suit/suit_i2c.h"
 
-/*
- * State of motor
- */
-typedef enum _MotorState
-{
-	MOTOR_STATE_OFF = 0,
-	MOTOR_STATE_ON
-} MotorState;
-
-/*
- * Motor error codes
- */
-typedef enum _MotorErrorCode
-{
-	//On success
-	MOTOR_E_SUCCESS = 0,
-	MOTOR_E_FAIL
-} MotorErrorCode;
-
-/**
- * Motor object;
- */
-typedef struct _Motor
-{
-	uint8_t intensity;
-	uint8_t calibrateComplete;
-} Motor;
-
 /* 
- * Calibrate selected motor;
- * Returns 0 on success; else see error code
+ * Init selected motor's driver
  */
-MotorErrorCode motor_calibrate(Motor *motor);
+void motor_init();
 /*
  * Run selected motor at specific intensity level ()
  */
-MotorErrorCode motor_run(Motor *motor, uint8_t intensity);
-/*
- * Stop selected motor
+void motor_run(uint8_t intensity);
+/* Run the internal waveform
+ * 1) PUNCH
  */
-MotorErrorCode motor_stop(Motor *motor);
-
+void motor_runWaveform();
 
 #endif /* MOTOR_H_ */
